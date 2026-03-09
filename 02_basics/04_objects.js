db7ec2ec48ac33100e79b7bcc52b0360e350041d
Kunal Prasad <kunal2912prasad@gmail.com>
2026-03-09 13:09:49 +0000

    objects

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,53 @@
+//const tinder = new Object()
+const tinderuser={}
+
+ tinderuser.id = "123abc"
+ tinderuser.name = "sam"
+ tinderuser.isloggedin = false
+
+//console.log(tinderuser);
+
+const regularuser = {
+    email: "kun@gmail.com",
+    fullname: {
+        username: {
+            firstname: "Kunal",
+            lastname: "Prasad"
+        }
+    }
+}
+// console.log(regularuser.fullname.username);
+
+const obj1 = {1: "a", 2:"b"}
+const obj2 = {3: "c", 4:"d"}
+const obj4 = {5: "c", 6:"d"}
+
+
+// const obj3 = {obj1,obj2}
+//const obj3= Object.assign({}, obj1, obj2, obj4)
+
+const obj3= {...obj1,...obj2}
+//console.log(obj3);
+
+
+const users = [
+    {
+        id: 1,
+        email: "k@hma.com"
+    },
+    {
+
+    },
+    {
+
+    }
+]
+
+users[1].email
+console.log(tinderuser);
+
+console.log(Object.keys(tinderuser));
+console.log(Object.values(tinderuser));
+console.log(Object.entries(tinderuser));
+
+console.log(tinderuser.hasOwnProperty('email'));
