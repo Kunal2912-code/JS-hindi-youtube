82ef5efdd41cfaac06fcdb710331c82eea34bf26
Kunal Prasad <kunal2912prasad@gmail.com>
2026-03-16 12:47:35 +0000

    for each loop

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -19,3 +19,22 @@ const coding = ["js", "ruby", "Cpp", "python"]
 // coding.forEach( (item, index, arr) => {
 //     console.log(item, index, arr);
 // })
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFileName: "JS"
+    },
+    {
+        languageName: "Java",
+        languageFileName: "Java"
+    },
+    {
+        languageName: "Python",
+        languageFileName: "PY"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.languageName);
+} )
